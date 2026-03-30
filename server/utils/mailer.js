a27686ee35e752902ccf51fa3936dacc05f343e2
unknown <nelsonfern83@gmail.com>
2026-03-30 12:07:38 -0300

    fix(mailer): force ipv4 connection

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,6 +1,11 @@
-import nodemailer from "nodemailer";
+import nodeMailer from "nodemailer";
+import dns from "node:dns";
 
-export const transporter = nodemailer.createTransport({
+// Fuerza a Node.js a utilizar redes IPv4 en lugar de IPv6 al enviar emails.
+// Esto soluciona de raíz el error ENETUNREACH en plataformas como Render que no soportan conexión saliente IPv6.
+dns.setDefaultResultOrder("ipv4first");
+
+export const transporter = nodeMailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false, // Render puede bloquear ciertas conexiones TLS por defecto, usar STARTTLS en puerto 587
